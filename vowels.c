9c1a123fc78f052f116279de66c7f3e9a7e95869
karthigaiselvi <35292744+karthigaiselvi@users.noreply.github.com>
2018-01-27 20:44:55 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+ #include<conio.h>
+ #include<string.h>
+ void main()
+ {
+ char x;
+   scanf("%c",&x);
+   if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
+   printf("vowels");
+   else
+   printf("constant");
+}
